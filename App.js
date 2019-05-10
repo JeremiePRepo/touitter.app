be5cafd65122ccfae01f4a87acf2d62b94ced1c0
Jérémie Pasquis <jeremie_DWWM@cefimmac13.pedago>
2019-05-10 14:23:28 +0200

    add trending

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
+/*\
+------------------------------------------------------------------------------
+App.js
+------------------------------------------------------------------------------
+Composant principal de l'application
+------------------------------------------------------------------------------
+\*/
+
+/*\
+Imports
+------------------------------------------------------------------------------
+\*/
 import React from 'react';
 import Header from './components/Header';
 import TrendingContainer from './components/TrendingContainer';
@@ -5,19 +17,27 @@ import { StyleSheet, ScrollView } from 'react-native';
 import TouitContainer from './components/TouitContainer';
 import SendMessageForm from './components/SendMessageForm';
 
+/*\
+Class
+------------------------------------------------------------------------------
+\*/
 export default class App extends React.Component {
   render() {
     return (
       <ScrollView style={styles.container}>
         <Header />
-        <TrendingContainer />
         <SendMessageForm />
         <TouitContainer />
+        <TrendingContainer />
       </ScrollView>
     );
   }
 }
 
+/*\
+Styles
+------------------------------------------------------------------------------
+\*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- components/TrendingContainer.js
+++ components/TrendingContainer.js
@@ -38,25 +38,14 @@ class TrendingContainer extends React.Component {
         this.updateTrendingContainer();
     }
 
+    /**
+     * Mets à jours la liste des mots-clés du trending
+     */
     updateTrendingContainer = () => {
         this.TouitApi = new TouitApi();
         this.TouitApi.httpGetTrending((response) => {
-            // console.log(response);
-            // this.setState({
-            //     trending_datas: response
-            // });
-            // console.log(Object.entries(response));
-
-
-            // Object.entries(response).forEach(element => {
-            //     this.setState({
-            //         trending_datas: [...this.state.touits_datas, ...element]
-            //     });
-            // });
-
             responseArray = Object.entries(response);
             formatResponse = [];
-
             for (let index = 0; index < responseArray.length; index++) {
                 const element = responseArray[index];
                 // console.log('element 0 : ' + element[0] + ' ; element 1 : ' + element[1] + ' ; index : ' + index);
@@ -67,37 +56,25 @@ class TrendingContainer extends React.Component {
                     }
                 );
             }
-            console.log(formatResponse);
+            formatResponse.sort(this.sortTrending);
             this.setState({
                 trending_datas: formatResponse,
             });
-            // console.log(this.state);
-
-
-
-            // this.setState({
-            //     trending_datas: Object.entries(response)
-            // });
-            // console.log(this.state);
-            // for (let index = 0; index < response.length; index++) {
-            //     // const element = array[index];
-            //     console.log(Object.entries(response)[index]);
-            // }
         })
     }
 
+    /**
+     * Tri descendant des éléments
+     */
+    sortTrending = (a, b) => {
+        return -(a.nb - b.nb);
+    }
+
+    /**
+     * Render du component
+     */
     render() {
         const { trending_datas } = this.state;
-        // let dataArray = [];
-
-        // for (let index = 0; index < trending_datas.length; index++) {
-        //     const element = [index => trending_datas[index]];
-        //     dataArray.push(element);
-        // }
-
-        // console.log(dataArray);
-
-
         return (
             <View style={styles.container}>
                 <FlatList
